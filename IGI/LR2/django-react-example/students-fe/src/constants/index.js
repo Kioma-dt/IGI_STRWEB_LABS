16838a7c0b9a1dfbfac4174b4126ad8c612bda0f
Avramenko Roman <www.kioma220@gmail.com>
2026-03-02 21:59:43 +0300

    Added NetWork And Volume

--- IGI/LR2/django-react-example/students-fe/src/constants/index.js
+++ IGI/LR2/django-react-example/students-fe/src/constants/index.js
@@ -1,1 +1,3 @@
-export const API_URL = "http://localhost:8000/api/students/";
+export const API_URL = `${process.env.REACT_APP_API_URL}/api/students/`
+
+// export const API_URL = "http://localhost:8000/api/students/";
